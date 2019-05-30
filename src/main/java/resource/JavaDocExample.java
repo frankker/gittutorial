49f8bd547b0a48d35a4a8a0834eeedcf70026a26
frankker8 <frankker@gmail.com>
2019-05-30 22:16:49 +0200

    git tutorial v0.1

---
+++ src/main/java/resource/JavaDocExample.java
@@ -0,0 +1,24 @@
+package resource;
+
+/**
+ * This is the example to learn
+ * Java Doc
+ * @author Frank Ker
+ */
+public class JavaDocExample {
+
+  /**
+   * This is the name of the person
+   */
+  private String name;
+
+  /**
+   *
+   * @param name This is the name of the person to set
+   * @return name of the person
+   */
+  public String setName(String name) {
+    return name;
+  }
+
+}
